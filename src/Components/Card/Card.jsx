7fad9a6860d4c6d5a6f8059bef6692acb88fd084
Abhishek Bhagat <abhishekb740@gmail.com>
2022-09-28 23:35:09 +0530

    new publish

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Card.css"
 const Card = (props)=>{
     return (
-
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,20 +1,64 @@
 import React from "react";
 import "./Projects.css";
 import Card from "../Card/Card";
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
 const Projects = () => {
   return (
-    <div>
+    <div >
       <h1 className="heading"> Projects</h1>
-    <Container className="projects-main container">
-      <Card title="testing" content="this card is being tested" />
-      <Card title="testing" content="this card is being tested" />
-      <Card title="testing" content="this card is being tested" />
-      <Card title="testing" content="this card is being tested" />
-      <Card title="testing" content="this card is being tested" />
-      <Card title="testing" content="this card is being tested" />
+      <Container>
+        <a 
+          target="_blank"
+          href="https://abhishekb740.github.io/Food-order-app/"
+        >
+          <Card
+            className=" proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="Food Order App" content="A simple Food Order Application"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://abhishekb740.github.io/Crypto_curr_tracker/"
+        >
+          <Card
+            className="proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="Crypto Currency Tracker" content="Live Crypto Currency price tracker"
+          />
+        </a>
+        <a target="_blank" href="https://abhishekb740.github.io/UserInfo/">
+          <Card
+            className="proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="User-Info" content="Displaying details of the User"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://abhishekb740.github.io/interactive-rating-component/" 
+        >
+          <Card
+            className="proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="Interactive Rating Component" content="A simple Rating component"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://abhishekb740.github.io/Huddle-landing-page/"
+        >
+          <Card
+            className="proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="Huddle Landing Page" content="A simple Huddle landing page"
+          />
+        </a>
+        <a
+        target="_blank" href="https://abhishekb740.github.io/NFT_Card/"
+        >
+          <Card
+            className="proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110"
+            title="NFT-Card" content="NFT-card Component"
+          />
+        </a>
 
-      {/* <div className="crypto_curr_tracker proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110" >
+        {/* <div className="crypto_curr_tracker proj log transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110" >
         <a target="_blank" href="https://abhishekb740.github.io/Crypto_curr_tracker/" >
           Crypto Currency Tracker
         </a>
@@ -67,7 +111,7 @@ const Projects = () => {
         </a>
       </div>
     </div> */}
-    </Container>
+      </Container>
     </div>
   );
 };
